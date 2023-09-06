cf0c378999b417ce1086977c08e2a9e7e1c040c5
Adham Yacout <Adham.yacout@gmail.com>
2023-09-06 13:42:50 +0300

    splash screen

---
+++ lib/home/homescreen.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class homescreen extends StatelessWidget {
+  static const String routename='Home';
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(color: Colors.white,
+
+
+    );
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:todolist/home/homescreen.dart';
+
+void main() {
+  runApp( MyApp());
+}
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(debugShowCheckedModeBanner: false,
+
+      routes:{ homescreen.routename:(context) => homescreen()},
+      initialRoute: homescreen.routename,
+
+    );
+  }
+}
